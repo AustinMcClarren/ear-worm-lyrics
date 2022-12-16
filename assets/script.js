18552eb49d83855cdb3ef17ed86877a0f3c1d8c7
Austin Mcclarren <115489580+AustinMcClarren@users.noreply.github.com>
2022-12-15 21:13:50 -0500

    started working on artist page

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//make a fetch for music match api
+//make a fetch for ticketmaster api
+//
+
+
+let music = {
+	ApiKey: "",
+	fetchMusic: function () {
+		fetch(
+			""
+		)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+				// console.log(data);
+				(data);
+				(data);
+			});
+	},
+};
