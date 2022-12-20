46f7de8d28b2f50dfb3e338e4772430bbb956398
Austin Mcclarren <115489580+AustinMcClarren@users.noreply.github.com>
2022-12-19 20:30:03 -0500

    made changes to java

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,10 @@
+//Grabbing elements from the DOM
+const artistInput = document.querySelector(".artist");
+const songInput = document.querySelector(".song");
+const albumInput = document.querySelector(".album");
+const genreInput = document.querySelector(".genre");
 
-//make a fetch for music match api
-//make a fetch for ticketmaster api
-//
+// const musicGrab
+// fetch(URL).then (function()) {
 
-
-let music = {
-	ApiKey: "",
-	fetchMusic: function () {
-		fetch(
-			""
-		)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (data) {
-				// console.log(data);
-				(data);
-				(data);
-			});
-	},
-};
-$('.search-button').click(function(){
-  $(this).parent().toggleClass('open');
-});
-
-
+// }
