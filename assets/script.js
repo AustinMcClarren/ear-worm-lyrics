303b0a3b23107ff317cf964d4a747482266011e7
Courtney <courtneyc017@gmail.com>
2022-12-20 21:32:15 -0500

    API and updating styles

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,18 @@
-
-//make a fetch for music match api
-//make a fetch for ticketmaster api
-//
-
-
-let music = {
-	ApiKey: "",
-	fetchMusic: function () {
-		fetch(
-			""
-		)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (data) {
-				// console.log(data);
-				(data);
-				(data);
-			});
-	},
-};
+//search button toggle
 $('.search-button').click(function(){
   $(this).parent().toggleClass('open');
 });
 
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e60a7d9611msh902330463102e34p11a394jsn1e2b6388637f',
+		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+	}
+};
+
+fetch('https://genius-song-lyrics1.p.rapidapi.com/search?q=Alan%20Walker&per_page=10&page=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
