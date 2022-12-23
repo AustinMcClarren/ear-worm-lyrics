51739749cce953eb555380c1b642ac9e386c8949
Austin Mcclarren <115489580+AustinMcClarren@users.noreply.github.com>
2022-12-22 21:10:23 -0500

    made the template card for artist page

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,1 @@
-//Grabbing elements from the DOM
-const artistInput = document.querySelector(".artist");
-const songInput = document.querySelector(".song");
-const albumInput = document.querySelector(".album");
-const genreInput = document.querySelector(".genre");
-
-// const musicGrab
-// fetch(URL).then (function()) {
-
-// }
+/* This changes all the animations globally */
